bc6e44a25dfa0e508bfa93fd340f8e3d63aa9c41
tejoyv <tejoyv@gmail.com>
2020-03-23 10:16:51 +0530

    User Account and Profile Picture

--- Flask_Learn/routes.py
+++ Flask_Learn/routes.py
@@ -1,6 +1,7 @@
+import secrets,os
 from flask import Flask,render_template,url_for,flash,redirect,request
 from Flask_Learn.models import User,Post
-from Flask_Learn.forms import RegisterationForm,LoginForm
+from Flask_Learn.forms import RegisterationForm,LoginForm,UpdateAccountForm
 from Flask_Learn import app,db,bcrypt   # from __init__.py
 from flask_login import login_user,current_user,logout_user,login_required
 
@@ -64,8 +65,31 @@ def logout():
     logout_user()
     return redirect (url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account",methods=['GET','POST'])
 @login_required
 def account():
-    return render_template('account.html',title='Account')
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file=picture_file
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash ('Your account has been updated !','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email
+    image_file=url_for('static',filename='profile_pics/'+current_user.image_file) # image_file from db column
+    return render_template('account.html',title='Account',image_file=image_file,form=form)
 
